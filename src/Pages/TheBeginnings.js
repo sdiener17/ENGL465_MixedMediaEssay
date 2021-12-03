1de91110fd4eda2cd4c111dc847c14d6bbb07237
sdiener17 <sdiener@ksu.edu>
2021-12-02 21:09:57 -0600

    Update

--- src/Pages/TheBeginnings.js
+++ src/Pages/TheBeginnings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react/cjs/react.development";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import background2 from "../Images/background2.jpg";
 import ReactAudioPlayer from "react-audio-player";
